add5e4078c47a7531900d287f9d51ce33de720be
Milena Moiteiro <60950057+milenamoiteiro001@users.noreply.github.com>
2020-03-03 22:15:14 -0300

    Add files via upload

---
+++ progressao_exponencial/120.py
@@ -0,0 +1,10 @@
+#autores: 1dsi3
+#@milena_moiteiro
+#feito em: 03/03/2020
+#instituição: ETEC de Itanhaém
+x=0 #declaração contador
+valor=2 #declaração da variavel valor
+while (x<=21)
+	valor=valor*valor
+	x=x+1 #laço de repetição
+print=valor #exibir a variavel valor
